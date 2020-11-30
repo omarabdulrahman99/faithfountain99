148bcd5b646e46eb4665d0af11bac60ed53ff73e
omarabdulrahman99 <saintnicks78@hotmail.com>
2020-11-29 21:02:08 -0500

    incr div vid height

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <div className="quote">
           Quran 75:36  "Does man think that he will be left neglected?"
         </div>
-        <div style={{width:"100%", height:"0px", position:"relative"}}>
+        <div style={{width:"100%", height:"900px", position:"relative"}}>
           <iframe src="https://streamable.com/e/whijq7?autoplay=1" frameborder="0" width="100%" height="100%" allowfullscreen allow="autoplay">
           </iframe>
         </div>
